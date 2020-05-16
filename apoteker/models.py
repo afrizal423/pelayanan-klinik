f38f8396de49d3068572b9baf780e12177af324e
afrizal423 <afrizal2499@gmail.com>
2020-05-16 21:50:00 +0700

    adding model

--- apoteker/models.py
+++ apoteker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from dokter.models import RekamMedis
 
 # Create your models here.
 class Obat(models.Model):
@@ -6,6 +7,20 @@ class Obat(models.Model):
     stok_obat = models.BigIntegerField()
     harga_obat = models.BigIntegerField()
     dosis_obat = models.IntegerField()
+    updated_on = models.DateTimeField(auto_now= True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.nama_obat
+
+class PemesananObat(models.Model):
+    idobat = models.ForeignKey(Obat,on_delete=models.CASCADE)
+    idrm = models.ForeignKey(RekamMedis,on_delete=models.CASCADE)
+    jumlah_obat = models.IntegerField()
+    subtotal_obat = models.BigIntegerField()
     updated_on = models.DateTimeField(auto_now= True)
     created_on = models.DateTimeField(auto_now_add=True)
     
--- dokter/models.py
+++ dokter/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from pegawaiadmin.models import Antrian, Pendaftaran
+from akun.models import User
 # Create your models here.
+class RekamMedis(models.Model):
+    idantrian = models.ForeignKey(Antrian,on_delete=models.CASCADE)
+    iddokter = models.ForeignKey(User,on_delete=models.CASCADE)
+    idpendaftaran = models.ForeignKey(Pendaftaran, on_delete=models.CASCADE)
+    diagnosa = models.TextField()
+    updated_on = models.DateTimeField(auto_now= True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return "{}. {}".format(self.idpendaftaran, self.diagnosa)
