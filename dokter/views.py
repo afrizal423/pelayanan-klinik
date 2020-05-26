0ac3f8c8aa26f52fe645ac1b1055107ad98845e7
afrizal423 <afrizal2499@gmail.com>
2020-05-26 16:41:07 +0700

    view dokter

--- dokter/views.py
+++ dokter/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect ,get_object_or_404
+from django.template.context import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from dokter.decorators import dokter_area
 from datetime import date
 from dokter.models import RekamMedis
+from apoteker.models import Obat
+from django.db.models import Q
 
 # Create your views here.
 @dokter_area
@@ -15,7 +18,7 @@ def index(request):
 
 @dokter_area
 def antrian(request):
-    hasil = RekamMedis.objects.all().select_related('idpendaftaran').filter(created_on__contains=date.today()).order_by('created_on')
+    hasil = RekamMedis.objects.all().select_related('idpendaftaran').filter(created_on__contains=date.today(), idantrian__is_dokterumum=True, idantrian__statusdokter="belum").order_by('created_on')
     data = {
         'sessionnya' : request.session['jenis_akun'],
         'namaakun' : request.session['namapegawai'],
@@ -24,3 +27,35 @@ def antrian(request):
     # print(hasil.get().idpendaftaran.norm.norm)
     # print(hasil.get().idantrian.noantrian)
     return render(request, 'hal_admin/dokter/antrian.html', data)
+
+@dokter_area
+def periksa(request , id):
+    obj = get_object_or_404(RekamMedis, id = id) 
+    print(obj.idpendaftaran.norm.norm)
+
+    item_id = str(1)
+    cart_items = request.session
+
+
+    # Create an empty cart object if it does not exist yet 
+    if not cart_items.has_key("cart"):
+        cart_items["cart"] = {}
+
+    print(cart_items["cart"])
+
+    data = {
+        'sessionnya' : request.session['jenis_akun'],
+        'namaakun' : request.session['namapegawai'],
+        'data': obj
+    }
+    return render(request, 'hal_admin/dokter/periksa.html', data)
+
+def cariobat(request):
+    if request.is_ajax():
+        q = request.GET.get('q')
+        if q is not None:            
+            results = Obat.objects.filter(  
+            	Q( nama_obat__contains = q ) )          
+            # print(results)
+            return render(request,'hal_admin/dokter/cariobat.html', {'results': results})
+
