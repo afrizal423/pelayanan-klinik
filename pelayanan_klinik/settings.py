232d3173b410ea5121be5df07aacdc433d27dfc8
afrizal423 <afrizal2499@gmail.com>
2020-08-06 06:45:44 +0700

    update Django v3.1

--- pelayanan_klinik/settings.py
+++ pelayanan_klinik/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import pymysql
-pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.version_info = (1, 4, 0, "final", 0)
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
